c5a4c9a1ed6bd7bd4b6b33345c72937cdec1be50
Ravi Resck <ravi@collabdesign.org>
2020-10-06 19:06:24 +0100

    a
    a

---
+++ flare.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="flare" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="../tiled/examples/sticker-knight/map/flare.png" width="192" height="192"/>
+</tileset>
---
+++ sign.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="sign" tilewidth="32" tileheight="32" tilecount="20" columns="5">
+ <image source="../tiled/examples/sticker-knight/map/sign.png" width="160" height="128"/>
+</tileset>
