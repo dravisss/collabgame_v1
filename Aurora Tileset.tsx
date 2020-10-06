74862b8a7f8cc53d3707d3671d87df5de90e374f
Ravi Resck <ravi@collabdesign.org>
2020-10-06 20:44:54 +0100

    a
    a

---
+++ Aurora Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Aurora Tileset" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="896" columns="28">
+ <image source="../../../Downloads/tilesets/Aurora Tileset.png" width="928" height="1088"/>
+</tileset>
